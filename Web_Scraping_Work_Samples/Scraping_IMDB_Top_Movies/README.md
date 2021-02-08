5d6a8fe2b0d4d3c4d358fa45dca45f1bfec9d02d
Shahul Hameed <shahulhameed19924@gmail.com>
2021-02-08 11:35:34 +0530

    scraped from multiple pages

--- Web_Scraping_Work_Samples/Scraping_IMDB_Top_Movies/README.md
+++ Web_Scraping_Work_Samples/Scraping_IMDB_Top_Movies/README.md
@@ -1,4 +1,8 @@
-I have scraped the IMDB data to get top movies with title, year and imdb rating using scrapy.
+I have scraped the IMDB data to get movies with title, year and imdb rating using scrapy from multiple pages and added spoofing headers.
+
+
+Library: scrapy
+
 
 Script file: imdb/spiders/top_movies.py
 
@@ -7,4 +11,3 @@ Output is saved as imdb_dataset.csv and imdb_dataset.json.
 
 
 
-Next step is to scrape from multiple pages and to clean the title column data.
--- Web_Scraping_Work_Samples/Scraping_IMDB_Top_Movies/imdb/spiders/top_movies.py
+++ Web_Scraping_Work_Samples/Scraping_IMDB_Top_Movies/imdb/spiders/top_movies.py
@@ -5,12 +5,26 @@ import scrapy
 class TopMoviesSpider(scrapy.Spider):
     name = 'top_movies'
     allowed_domains = ['www.imdb.com/']
-    start_urls = ['https://www.imdb.com/chart/top/']
+    # start_urls = ['https://www.imdb.com/search/title/?genres=action,drama/']
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.imdb.com/search/title/?genres=action,drama/', callback=self.parse,headers={
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36'
+        })
 
     def parse(self, response):
-        for movie in response.xpath("//tbody[@class='lister-list']/tr"):            
-            yield {
-                'title':movie.xpath(".//td[@class='titleColumn']/a/text()").get(),
-                'year':movie.xpath(".//td[@class='titleColumn']/span/text()").get(),
-                'imdb_rating':movie.xpath(".//td[@class='ratingColumn imdbRating']/strong/text()").get()
+        for movie in response.xpath("//div[@class='lister-list']/div"):
+            yield{
+                'movie_name':movie.xpath(".//h3/a/text()").get(),
+                'year':movie.xpath(".//h3/span[@class='lister-item-year text-muted unbold']/text()").get(),
+                'imdb_rating':movie.xpath(".//div[@class='inline-block ratings-imdb-rating']/strong/text()").get(),
+                'votes':movie.xpath(".//p[@class='sort-num_votes-visible']/span[2]/text()").get()
             }
+
+        next_page=response.xpath("(//a[@class='lister-page-next next-page'][1]/@href)[2]").get()
+        print(next_page)
+
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse,headers={
+                'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36'
+        })
